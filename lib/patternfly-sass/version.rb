ced743ba614edac8c856e7e983ae812ebe255984
Dávid Halász <dhalasz@redhat.com>
2016-04-29 16:06:04 +0200

    Converted from a special branch made with tertiary navigation

--- lib/patternfly-sass/version.rb
+++ lib/patternfly-sass/version.rb
@@ -1,4 +1,4 @@
 module Patternfly
-  VERSION = '3.3.5'
-  PATTERNFLY_SHA = 'fc42733bff8d04f603513809bd011c60e40f0c1b'
+  VERSION = '3.3.5-tertiary'
+  PATTERNFLY_SHA = '0abc260a8d026d9b2e4e455b58e100a7b5424d05'
 end
